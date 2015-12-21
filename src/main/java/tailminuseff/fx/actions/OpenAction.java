f441bd4476b0de069bf3694de345f392762286a4
Vincent Cheesbrough <vincent.cheesbrough@gmail.com>
2015-12-21 16:22:37 +0000

    Open dialog now uses and updates and uses the open files dialog directory from config. Closes #4

--- src/main/java/tailminuseff/fx/actions/OpenAction.java
+++ src/main/java/tailminuseff/fx/actions/OpenAction.java
@@ -9,6 +9,7 @@ import javafx.stage.Stage;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import org.controlsfx.control.action.Action;
+import tailminuseff.config.Configuration;
 import tailminuseff.fx.FileOpenCommand;
 
 @Singleton
@@ -16,20 +17,26 @@ public class OpenAction extends Action {
 
     private final Stage stage;
     private final EventBus eventBus;
+    private final Configuration config;
 
     @Inject
-    public OpenAction(Stage stage, EventBus eventBus) {
+    public OpenAction(Stage stage, EventBus eventBus, Configuration config) {
         super("Open …");
         setEventHandler(this::exec);
         this.stage = stage;
         this.eventBus = eventBus;
+        this.config = config;
     }
 
     public void exec(ActionEvent evt) {
         FileChooser fileChooser = new FileChooser();
+        if (config.getOpenDialogDirectory().exists()) {
+            fileChooser.setInitialDirectory(config.getOpenDialogDirectory());
+        }
         fileChooser.setTitle("Open Files");
         List<File> files = fileChooser.showOpenMultipleDialog(stage);
-        if (files != null) {
+        if (files != null && files.size() > 0) {
+            config.setOpenDialogDirectory(files.get(0).getParentFile());
             files.forEach(f -> eventBus.post(new FileOpenCommand(f)));
         }
     }
