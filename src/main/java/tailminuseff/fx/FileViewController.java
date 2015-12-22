06204642a34bb4d9a36eb070f1a9f1d8659ee123
Vincent Cheesbrough <vincent.cheesbrough@gmail.com>
2015-12-22 17:27:30 +0000

    Basic layout for searching

--- src/main/java/tailminuseff/fx/FileViewController.java
+++ src/main/java/tailminuseff/fx/FileViewController.java
@@ -11,8 +11,9 @@ import java.util.concurrent.Future;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.layout.VBox;
 import javax.inject.Inject;
-import org.fxmisc.richtext.CodeArea;
+import org.fxmisc.richtext.InlineStyleTextArea;
 import org.fxmisc.richtext.LineNumberFactory;
 import org.fxmisc.richtext.NavigationActions;
 import tailminuseff.FileExectutor;
@@ -25,28 +26,15 @@ import tailminuseff.io.LineAddedEvent;
 public class FileViewController implements Initializable {
 
     @FXML
-    private CodeArea text;
+    private VBox textContainer;
 
+    private final InlineStyleTextArea<InlineTextStyle> textArea;
     private final ExecutorService executorService;
     private final FileMonitor fileMonitor;
     private final EventBus eventBus;
     private final File file;
     private Future<Void> future;
 
-    private final FileMonitorListener monitorListener = new FileMonitorListener() {
-        @Override
-        public void fileReset(FileResetEvent evt) {
-            Platform.runLater(() -> text.clear());
-        }
-
-        @Override
-        public void lineRead(LineAddedEvent evt) {
-            Platform.runLater(() -> {
-                text.appendText(text.getLength() > 0 ? "\n" + evt.getLine() : evt.getLine());
-                text.lineStart(NavigationActions.SelectionPolicy.CLEAR);
-            });
-        }
-    };
     @Inject
     FileViewController(@Assisted File file, FileMonitorFactory fileMonitorFactory, @FileExectutor ExecutorService executorService, EventBus eventBus) {
         this.fileMonitor = fileMonitorFactory.createForFile(file);
@@ -54,17 +42,41 @@ public class FileViewController implements Initializable {
         this.executorService = executorService;
         this.eventBus = eventBus;
         this.file = file;
+        this.textArea = new InlineStyleTextArea<>(new InlineTextStyle(), (style) -> style.toCSS());
     }
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        text.setParagraphGraphicFactory(LineNumberFactory.get(text));
-        text.setWrapText(false);
+        textContainer.getChildren().add(this.textArea);
+        textArea.setParagraphGraphicFactory(LineNumberFactory.get(textArea));
+        textArea.setWrapText(false);
         future = executorService.submit(fileMonitor);
     }
 
     public void closed() {
         future.cancel(true);
         Logger.getLogger(this.getClass()).finer("Closed {0}", new Object[]{file});
+    }
+
+    private final FileMonitorListener monitorListener = new FileMonitorListener() {
+        @Override
+        public void fileReset(FileResetEvent evt) {
+            Platform.runLater(() -> textArea.clear());
+        }
+
+        @Override
+        public void lineRead(LineAddedEvent evt) {
+            Platform.runLater(() -> {
+                textArea.appendText(textArea.getLength() > 0 ? "\n" + evt.getLine() : evt.getLine());
+                textArea.lineStart(NavigationActions.SelectionPolicy.CLEAR);
+            });
+        }
+    };
+
+    private static class InlineTextStyle {
+
+        public String toCSS() {
+            return "";
+        }
     }
 }
